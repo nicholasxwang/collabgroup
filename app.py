d5a3631f13db4fbdc00e82ca78cdeeba7ffa87f4
nicholasxwang <80929436+nicholasxwang@users.noreply.github.com>
2021-10-30 08:35:52 -0700

    new update

--- app.py
+++ app.py
@@ -72,3 +72,6 @@ def home():
     with open("main.json","r") as out:
         file = json.load(out)
     return str([time.time() - start_time,file])
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
